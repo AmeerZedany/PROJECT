3cefe1ebca2f683568ae8222757cac0d8580fa42
AmeerZedany <ameer.zedane2@gmail.com>
2025-03-19 19:26:38 +0200

    F

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: "/PROJECT/", 
+  base: "/PROJECT/", // Replace PROJECT with your repository name
 });
